038182dee07f5d7d551396b6a1e744ee972c2d57
Gokulaa2311 <78991586+Gokulaa2311@users.noreply.github.com>
2021-05-11 18:53:23 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -23,14 +23,14 @@ menu_list_array.sort();
 htmldata="<section class='cards'>"
 for(var i=0;i<menu_list_array.length;i++){
 
-    htmldata=htmldata+'<div class="cards">'
+    htmldata=htmldata+'<div class="card">'
     + '<img src="https://image.freepik.com/free-vector/pizza-slice-cartoon-illustration_138676-1497.jpg"'
     +menu_list_array[i] + '</div>'
 
 }
 
 htmldata=htmldata+"</section>"
-document.getElementById("display_addmenu").innerHTML=htmldata;
+document.getElementById("display_addedmenu").innerHTML=htmldata;
 }
 
 function add_top(){
